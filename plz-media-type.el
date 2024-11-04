c0a109307010c275706892a0a3152835c383aedc
Roman Scherer <roman@burningswell.com>
2024-11-04 20:31:37 +0100

    Release 0.2.2

--- plz-media-type.el
+++ plz-media-type.el
@@ -5,7 +5,7 @@
 ;; Author: r0man <roman@burningswell.com>
 ;; Maintainer: r0man <roman@burningswell.com>
 ;; URL: https://github.com/r0man/plz-media-type
-;; Version: 0.2.2-pre
+;; Version: 0.2.2
 ;; Package-Requires: ((emacs "26.3") (plz "0.9"))
 ;; Keywords: comm, network, http
 
